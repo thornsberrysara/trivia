4904d9b324804453c367dc2a9d11187f3b73e87a
Sara Thornsberry <thornsberrysara@gmail.com>
2020-09-30 21:18:19 -0400

    Updates

---
+++ precache-manifest.59bcef0599bfd3ad93818832e7fe0db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99f2f46d84eea4d2a25bc495b8e2118",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "0ed3e2df13135d34b03b",
+    "url": "/trivia/static/css/main.6fbafc03.chunk.css"
+  },
+  {
+    "revision": "ed9ea346e3ff48e375c0",
+    "url": "/trivia/static/js/2.4ad7820e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trivia/static/js/2.4ad7820e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed3e2df13135d34b03b",
+    "url": "/trivia/static/js/main.3dc3657e.chunk.js"
+  },
+  {
+    "revision": "194729bae84278f9686e",
+    "url": "/trivia/static/js/runtime-main.82c86a55.js"
+  }
+]);
